bc5faf25b6874a80ba0d118cf66a777407d36f45
Lucas Legname <lucas.legname@gmail.com>
2014-10-28 00:11:49 +0100

    Start implementation of the card wallet generator.

--- src/ninja.cardwallet.js
+++ src/ninja.cardwallet.js
@@ -9,55 +9,42 @@ ninja.wallets.cardwallet = {
 
 	// Verify that a self-entered key is valid, and compute the corresponding
 	// public address, render the wallet.
-	testAndApplyVanityKey: function () {
-		var suppliedKey = document.getElementById('suppliedPrivateKey').value;
+	generate: function () {
+		var suppliedKey = document.getElementById('suppliedPublicAddress').value;
 		suppliedKey = suppliedKey.trim(); // in case any spaces or whitespace got pasted in
-		document.getElementById('suppliedPrivateKey').value = suppliedKey;
-		if (!ninja.privateKey.isPrivateKey(suppliedKey)) {
-			alert(ninja.translator.get("detailalertnotvalidprivatekey"));
-		} else {
-			var computedPublicAddress = new Bitcoin.ECKey(suppliedKey).getBitcoinAddress();
-			if (ninja.wallets.paperwallet.encrypt) {
-				document.getElementById("busyblock").className = "busy";
-				ninja.privateKey.BIP38PrivateKeyToEncryptedKeyAsync(suppliedKey,
-					document.getElementById('paperpassphrase').value, false, function(encodedKey) {
-					document.getElementById("busyblock").className = "";
-					ninja.wallets.paperwallet.showArtisticWallet(1, computedPublicAddress, encodedKey);
-				});
-			}
-			else {
-				ninja.wallets.paperwallet.showArtisticWallet(1, computedPublicAddress, suppliedKey);
-			}
+		document.getElementById('suppliedPublicAddress').value = suppliedKey;
+
+		document.getElementById('cardkeyarea').innerHTML = "";
+
+		//ninja.wallets.paperwallet.showArtisticWallet(1, suppliedKey);
+		var i = 0;
+		for(i=0;i<9;i++)
+		{
+			document.getElementById('cardkeyarea').innerHTML += ninja.wallets.cardwallet.templateArtisticHtml(i);
+			ninja.wallets.cardwallet.showArtisticWallet(i, suppliedKey);
 		}
+		
 	},
 
 	templateArtisticHtml: function (i) {
-		var keyelement = 'btcprivwif';
+		var keyelement = 'card_btcprivwif';
 		var coinImgUrl = "logos/" + janin.selectedCurrency.name.toLowerCase() + ".png";
-		var walletBackgroundUrl = "wallets/" + janin.selectedCurrency.name.toLowerCase() + ".png";
+		var walletBackgroundUrl = "business_cards/" + janin.selectedCurrency.name.toLowerCase() + ".png";
 
 		var walletHtml =
-							"<div class='coinIcoin'> <img id='coinImg' src='" + coinImgUrl + "' alt='currency_logo' /></div><div class='artwallet' id='artwallet" + i + "'>" +
-								"<img id='papersvg" + i + "' class='papersvg' src='" + walletBackgroundUrl + "' />" +
-								"<div id='qrcode_public" + i + "' class='qrcode_public'></div>" +
-								"<div id='qrcode_private" + i + "' class='qrcode_private'></div>" +
-								"<div class='btcaddress' id='btcaddress" + i + "'></div>" +
-								"<div class='" + keyelement + "' id='" + keyelement + i + "'></div>" +
-								"<div class='paperWalletText'><img class='backLogo' src='" + coinImgUrl + "' alt='currency_logo' />" + ninja.translator.get("paperwalletback") + "</div>" +
+							"<div class='artcard' id='artcard" + i + "'><div class='cardCoinIcoin'> <img id='coinImg' src='" + coinImgUrl + "' alt='currency_logo' /></div>" +
+								"<img id='cardsvg" + i + "' class='cardsvg' src='" + walletBackgroundUrl + "' />" +
+								"<div id='card_qrcode_public" + i + "' class='card_qrcode_public'></div>" +
+								"<div class='card_btcaddress' id='card_btcaddress" + i + "'></div>" +
 							"</div>";
 		return walletHtml;
 	},
 
-	showArtisticWallet: function (idPostFix, bitcoinAddress, privateKey) {
+	showArtisticWallet: function (idPostFix, bitcoinAddress) {
 		var keyValuePair = {};
-		keyValuePair["qrcode_public" + idPostFix] = bitcoinAddress;
-		ninja.qrCode.showQrCode(keyValuePair, 3.5);
-		
-        var keyValuePair = {};
-        keyValuePair["qrcode_private" + idPostFix] = privateKey;
-        ninja.qrCode.showQrCode(keyValuePair, 2.8);
+		keyValuePair["card_qrcode_public" + idPostFix] = bitcoinAddress;
+		ninja.qrCode.showQrCode(keyValuePair, 3);
         
-        document.getElementById("btcaddress" + idPostFix).innerHTML = bitcoinAddress;
-		document.getElementById("btcprivwif" + idPostFix).innerHTML = privateKey;
+        document.getElementById("card_btcaddress" + idPostFix).innerHTML = bitcoinAddress;
 	},
 };
