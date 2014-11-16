dabee183cc6159c584ceb4b1ae5c5a3b51aaee32
Lucas Legname <lucas.legname@gmail.com>
2014-11-16 20:45:51 +0100

    Improvement of the card generator.

--- src/ninja.cardwallet.js
+++ src/ninja.cardwallet.js
@@ -14,19 +14,23 @@ ninja.wallets.cardwallet = {
 		suppliedKey = suppliedKey.trim(); // in case any spaces or whitespace got pasted in
 		document.getElementById('suppliedPublicAddress').value = suppliedKey;
 
+		var suppliedCustomText = document.getElementById('suppliedCustomText').value;
 		document.getElementById('cardkeyarea').innerHTML = "";
 
-		//ninja.wallets.paperwallet.showArtisticWallet(1, suppliedKey);
+		var keyValuePair = {};
+
 		var i = 0;
 		for(i=0;i<9;i++)
 		{
-			document.getElementById('cardkeyarea').innerHTML += ninja.wallets.cardwallet.templateArtisticHtml(i);
-			ninja.wallets.cardwallet.showArtisticWallet(i, suppliedKey);
+			document.getElementById('cardkeyarea').innerHTML += ninja.wallets.cardwallet.templateArtisticHtml(i, suppliedCustomText);
+			document.getElementById("card_btcaddress" + i).innerHTML = suppliedKey;
+			keyValuePair["card_qrcode_public" + i] = suppliedKey;
 		}
-		
+
+		ninja.qrCode.showQrCode(keyValuePair, 3.2);
 	},
 
-	templateArtisticHtml: function (i) {
+	templateArtisticHtml: function (i, customText) {
 		var keyelement = 'card_btcprivwif';
 		var coinImgUrl = "logos/" + janin.selectedCurrency.name.toLowerCase() + ".png";
 		var walletBackgroundUrl = "business_cards/" + janin.selectedCurrency.name.toLowerCase() + ".png";
@@ -36,15 +40,8 @@ ninja.wallets.cardwallet = {
 								"<img id='cardsvg" + i + "' class='cardsvg' src='" + walletBackgroundUrl + "' />" +
 								"<div id='card_qrcode_public" + i + "' class='card_qrcode_public'></div>" +
 								"<div class='card_btcaddress' id='card_btcaddress" + i + "'></div>" +
+								"<div class='card_custom_text' id='card_custom_text" + i + "'><span>" + customText + "</span></div>" +
 							"</div>";
 		return walletHtml;
-	},
-
-	showArtisticWallet: function (idPostFix, bitcoinAddress) {
-		var keyValuePair = {};
-		keyValuePair["card_qrcode_public" + idPostFix] = bitcoinAddress;
-		ninja.qrCode.showQrCode(keyValuePair, 3);
-        
-        document.getElementById("card_btcaddress" + idPostFix).innerHTML = bitcoinAddress;
 	},
 };
