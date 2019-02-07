868ea5074c42f2cbbd2f662ba613749d3404b67f
Utopianer <domnews@gmx.de>
2019-02-07 21:01:36 +0100

    Update

--- src/janin.currency.js
+++ src/janin.currency.js
@@ -86,7 +86,7 @@ janin.currency = {
 
 janin.currencies = [
     //                              name, networkVersion, privateKeyPrefix, WIF_Start, CWIF_Start, donate
-
+    janin.currency.createCurrency ("Bitcoin",             0x00, 0x80, "5",    "[LK]" , "15DHZzv7eBUwss77qczZiL3DUEZLjDYhbM"),
+    janin.currency.createCurrency ("Dogecoin",            0x1e, 0x9e, "6",    "Q"    , "D74Npoqhwhjw9fShkm5wbj6DD2BJXpmzPj"),
     janin.currency.createCurrency ("Domocoin",            0x1e, 0xba, "7",    "U"    , "")
-
                    ];
